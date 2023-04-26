ef7c1987ef64cc5ca5f0b17da092718594029ba8
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-26 16:01:27 -0300

    refatorei pra funcional

--- my-counter/src/App.js
+++ my-counter/src/App.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = {
-    counter: 0,
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const logica = () => {
+    setCounter(counter + 1)
   }
 
-  incrementCounter = () => {
-    const { counter } = this.state;
-
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1,
-    }))
-
-    this.setState({
-      counter: counter + 1,
-    })
-
-    // 2 jeitos de fazer.
-  }
-
-  render() {
-    const { counter } = this.state;
-    return (
-      <div>
-        <h1>Contador</h1>
-        <b>{counter}</b>
-        <br></br>
-        <button
-        onClick={ this.incrementCounter }
-        >Acrescentar 1</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Contador</h1>
+      <b>Contador: { counter }</b>
+      <br></br>
+      <button
+      onClick={ logica }
+      >Acrescentar 1</button>
+    </div>
+  )
 }
 
 export default App;
