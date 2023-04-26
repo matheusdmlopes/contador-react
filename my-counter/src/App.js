06b89c41792c448fd2c89036604b706b226ee181
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-26 15:35:56 -0300

    feito o contador com componente de classe normal

--- my-counter/src/App.js
+++ my-counter/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0,
+  }
+
+  incrementCounter = () => {
+    const { counter } = this.state;
+
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }))
+
+    this.setState({
+      counter: counter + 1,
+    })
+
+    // 2 jeitos de fazer.
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <h1>Contador</h1>
+        <b>{counter}</b>
+        <br></br>
+        <button
+        onClick={ this.incrementCounter }
+        >Acrescentar 1</button>
+      </div>
+    );
+  }
 }
 
 export default App;
