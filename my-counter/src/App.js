9f6a1a0eac4fffd62f089639589c1f40767898c1
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-05-12 20:54:38 -0300

    feito

--- my-counter/src/App.js
+++ my-counter/src/App.js
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
+import './/App.css'
 
 function App() {
   const [counter, setCounter] = useState(0);
 
-  const logica = () => {
+  const incrementOne = () => {
     setCounter(counter + 1)
   }
 
+  const incrementThree = () => {
+    setCounter(counter + 3)
+  }
+
+  const incrementFive = () => {
+    setCounter(counter + 5)
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
   return (
-    <div>
-      <h1>Contador</h1>
-      <b>Contador: { counter }</b>
+    <div className='App' >
+      <h1 id='counter-title'>Contador</h1>
+      <p id='counter-value'>Contador: {counter}</p>
       <br></br>
-      <button
-      onClick={ logica }
-      >Acrescentar 1</button>
-    </div>
+      <button className='buttons'
+        onClick={incrementOne}
+      >Acrescentar 1
+      </button>
+      <button className='buttons'
+        onClick={incrementThree}
+      >Acrescentar 3
+      </button>
+      <button className='buttons'
+        onClick={incrementFive}
+      >Acrescentar 5
+      </button>
+      <button className='buttons'
+        onClick={reset}
+      >Resetar
+      </button>
+    </div >
   )
 }
 
